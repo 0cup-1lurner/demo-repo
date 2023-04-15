d20f4c165dbd53176be31aeba45398fb0a928e49
Empty-Cup <neelalohitha.k@geeklurn.com>
2023-04-15 11:30:38 +0530

    Add comments to pset-2.py

--- python-class-GL/pset-2.py
+++ python-class-GL/pset-2.py
@@ -1,5 +1,5 @@
 """Solution for pset-2
 """
 
-name = input("Enter the name")
-print("Hello "+name +"! Welcome to the class!")
+name = input("Enter the name")  # Get input
+print("Hello "+name +"! Welcome to the class!")  # Concatenate
